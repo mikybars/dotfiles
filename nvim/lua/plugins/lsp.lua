2ff155688ba8257374e53e9583e3d0d1d3fce859
mikybars <mperezibars@gmail.com>
2025-04-23 07:30:14 +0200

    feat(nvim): display errors as virtual text

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,15 @@ return {
     "nvim-lspconfig",
     opts = {
       diagnostics = {
-        virtual_text = false,
+        virtual_text = {
+          source = "always",
+          prefix = "■",
+          -- Only show virtual text matching the given severity
+          severity = {
+            -- Specify a range of severities
+            min = vim.diagnostic.severity.ERROR,
+          },
+        },
       },
     },
   },
